fb267b87ed08b221ad1199273180720a313d296b
CedriCazanove <cedricazanove@hotmail.fr>
2022-02-05 20:51:19 +0100

    design done

--- src/View/ElevatorView.java
+++ src/View/ElevatorView.java
@@ -1,9 +1,11 @@
 package View;
 
+import java.awt.*;
 import java.util.Hashtable;
 import java.util.TimerTask;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -46,6 +48,8 @@ public class ElevatorView {
 
 	private JSlider sliderPOv_crawlSelect;
 
+	Border emptyBorder = BorderFactory.createEmptyBorder();
+
 	private Ressource rsc = new Ressource();
 
 	/**
@@ -73,7 +77,7 @@ public class ElevatorView {
 				System.out.println("Waiting for the state of the elevator..");
 			}
 		};
-		//timer.schedule(timerTask, 0, 5000);//period is in ms (every 5sec we ask)
+		timer.schedule(timerTask, 0, 5000);//period is in ms (every 5sec we ask)
 /*
 		while(!elevator.getPIm_ready()) {
 		}
@@ -90,7 +94,7 @@ public class ElevatorView {
 				new SendServPReq(mqttPublisher).actionPerformed(null);
 			}
 		};
-		//timer.schedule(timerTask, 0, 60000);//period is in ms (every 1min we ask)
+		timer.schedule(timerTask, 0, 60000);//period is in ms (every 1min we ask)
 	}
 
 	/**
@@ -98,18 +102,27 @@ public class ElevatorView {
 	 */
 	private void initialize(MqttPublisher mqttPublisher, Password password) {
 		frame = new JFrame("Elevator");
-		//frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setBounds(100, 100, 540, 660);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		/**
+		 * Panel as a fond of the software
+		 */
+		JPanel panelFont = new JPanel();
+		panelFont.setBounds(0,0, 540, 660);
+		panelFont.setLayout(null);
+		panelFont.setBackground(Color.white);
+		frame.add(panelFont);
+
+		/**
 		 * Panel to display the elevator
 		 */
 		JPanel panelElevator = new JPanel();
 		panelElevator.setBounds(30, 63, 100, 543);
-		frame.getContentPane().add(panelElevator);
+		panelFont.add(panelElevator);
+		panelElevator.setBackground(Color.white);
 		panelElevator.setLayout(null);
 
 		labelElevator = new JLabel(rsc.getIconElevatorClose());
@@ -118,7 +131,7 @@ public class ElevatorView {
 
 		JPanel panelFloor = new JPanel();
 		panelFloor.setBounds(10, 63, 10, 543);
-		frame.getContentPane().add(panelFloor);
+		panelFont.add(panelFloor);
 		panelFloor.setLayout(null);
 
 		lblFloorIndicator = new JLabel(rsc.getIconFloorIndicator());
@@ -127,67 +140,75 @@ public class ElevatorView {
 
 		//Button to call the elevator to go up at the level 0
 		btnUp = new JButton(rsc.getIconUp());
+		btnUp.setBorder(emptyBorder);
 		btnUp.setBounds(150, 512, 50, 50);
 		btnUp.setFocusPainted(false);
 		btnUp.setFocusable(false);
 		btnUp.addActionListener(new SendTravReq(mqttPublisher, "1UP"));
-		frame.getContentPane().add(btnUp);
+		panelFont.add(btnUp);
 
 		//Button to call the elevator to go up at the level 1
 		btnUp_1 = new JButton(rsc.getIconUp());
+		btnUp_1.setBorder(emptyBorder);
 		btnUp_1.setBounds(150, 347, 50, 50);
 		btnUp_1.setFocusPainted(false);
 		btnUp_1.setFocusable(false);
 		btnUp_1.addActionListener(new SendTravReq(mqttPublisher, "2UP"));
-		frame.getContentPane().add(btnUp_1);
+		panelFont.add(btnUp_1);
 
 		//Button to call the elevator to go down at the level 1
 		btnDown_1 = new JButton(rsc.getIconDown());
+		btnDown_1.setBorder(emptyBorder);
 		btnDown_1.setBounds(150, 408, 50, 50);
 		btnDown_1.setFocusPainted(false);
 		btnDown_1.setFocusable(false);
 		btnDown_1.addActionListener(new SendTravReq(mqttPublisher, "2DOWN"));
-		frame.getContentPane().add(btnDown_1);
+		panelFont.add(btnDown_1);
 
 		//Button to call the elevator to go up at the level 2
 		btnUp_2 = new JButton(rsc.getIconUp());
+		btnUp_2.setBorder(emptyBorder);
 		btnUp_2.setBounds(150, 212, 50, 50);
 		btnUp_2.setFocusPainted(false);
 		btnUp_2.setFocusable(false);
 		btnUp_2.addActionListener(new SendTravReq(mqttPublisher, "3UP"));
-		frame.getContentPane().add(btnUp_2);
+		panelFont.add(btnUp_2);
 
 		//Button to call the elevator to go down at the level 2
 		btnDown_2 = new JButton(rsc.getIconDown());
+		btnDown_2.setBorder(emptyBorder);
 		btnDown_2.setBounds(150, 273, 50, 50);
 		btnDown_2.setFocusPainted(false);
 		btnDown_2.setFocusable(false);
 		btnDown_2.addActionListener(new SendTravReq(mqttPublisher, "3DOWN"));
-		frame.getContentPane().add(btnDown_2);
+		panelFont.add(btnDown_2);
 
 		//Button to call the elevator to go down at the level 3
 		btnDown_3 = new JButton(rsc.getIconDown());
+		btnDown_3.setBorder(emptyBorder);
 		btnDown_3.setBounds(150, 108, 50, 50);
 		btnDown_3.setFocusPainted(false);
 		btnDown_3.setFocusable(false);
 		btnDown_3.addActionListener(new SendTravReq(mqttPublisher, "4DOWN"));
-		frame.getContentPane().add(btnDown_3);
+		panelFont.add(btnDown_3);
 
 		//Label to indicate the position of the elevator
 		lblElevatorIndicator = new JLabel(rsc.getIconElevatorOff());
 		lblElevatorIndicator.setBounds(10, 10, 190, 40);
-		frame.getContentPane().add(lblElevatorIndicator);
+		panelFont.add(lblElevatorIndicator);
 
 		/**
 		JPanel for the button inside the elevator available for the user
 		 */
 		JPanel panel = new JPanel();
 		panel.setBounds(230, 250, 110, 230);
-		frame.getContentPane().add(panel);
+		panelFont.add(panel);
+		panel.setBackground(Color.white);
 		panel.setLayout(null);
 
 		//Button to go at the level 0 from inside the elevator
 		btnLevel0 = new JButton(rsc.getIconLevel0());
+		btnLevel0.setBorder(emptyBorder);
 		btnLevel0.setBounds(0, 0, 50, 50);
 		panel.add(btnLevel0);
 		btnLevel0.setFocusPainted(false);
@@ -196,6 +217,7 @@ public class ElevatorView {
 
 		//Button to go at the level 1 from inside the elevator
 		btnLevel1 = new JButton(rsc.getIconLevel1());
+		btnLevel1.setBorder(emptyBorder);
 		btnLevel1.setBounds(60, 0, 50, 50);
 		panel.add(btnLevel1);
 		btnLevel1.setFocusable(false);
@@ -204,6 +226,7 @@ public class ElevatorView {
 
 		//Button to go at the level 2 from inside the elevator
 		btnLevel2 = new JButton(rsc.getIconLevel2());
+		btnLevel2.setBorder(emptyBorder);
 		btnLevel2.setBounds(0, 60, 50, 50);
 		panel.add(btnLevel2);
 		btnLevel2.setFocusPainted(false);
@@ -212,6 +235,7 @@ public class ElevatorView {
 
 		//Button to go at the level 3 from inside the elevator
 		btnLevel3 = new JButton(rsc.getIconLevel3());
+		btnLevel3.setBorder(emptyBorder);
 		btnLevel3.setBounds(60, 60, 50, 50);
 		panel.add(btnLevel3);
 		btnLevel3.setFocusable(false);
@@ -220,6 +244,7 @@ public class ElevatorView {
 
 		//Button to stop the elevator from inside
 		btnStop = new JButton(rsc.getIconStop());
+		btnStop.setBorder(emptyBorder);
 		btnStop.setBounds(30, 120, 50, 50);
 		panel.add(btnStop);
 		btnStop.setFocusable(false);
@@ -228,6 +253,7 @@ public class ElevatorView {
 
 		//Button to open the door of the elevator
 		btnOpen = new JButton(rsc.getIconOpen());
+		btnOpen.setBorder(emptyBorder);
 		btnOpen.setBounds(0, 180, 50, 50);
 		panel.add(btnOpen);
 		btnOpen.setFocusable(false);
@@ -236,6 +262,7 @@ public class ElevatorView {
 
 		//Button to close the door of the elevator
 		btnClose = new JButton(rsc.getIconClose());
+		btnClose.setBorder(emptyBorder);
 		btnClose.setBounds(60, 180, 50, 50);
 		panel.add(btnClose);
 		btnClose.setFocusable(false);
@@ -246,87 +273,101 @@ public class ElevatorView {
 		 * Supervisor component
 		 */
 		supervisorOnOff = new JToggleButton(rsc.getIconUser());
+		supervisorOnOff.setBorder(emptyBorder);
 		supervisorOnOff.addActionListener(new SupervisorOnOffAction(this, mqttPublisher));
 		supervisorOnOff.setBounds(390, 10, 120, 21);
-		frame.getContentPane().add(supervisorOnOff);
+		panelFont.add(supervisorOnOff);
 
 		/**
 		 * Supervisor Code to auth
 		 */
 		panelCodeAccessSupervisor = new JPanel();
 		panelCodeAccessSupervisor.setBounds(390, 31, 120, 180);
-		frame.getContentPane().add(panelCodeAccessSupervisor);
+		panelFont.add(panelCodeAccessSupervisor);
+		panelCodeAccessSupervisor.setBackground(Color.white);
 		panelCodeAccessSupervisor.setVisible(false);
 		panelCodeAccessSupervisor.setLayout(null);
 
 		//Button for the digit 1
 		btnDigit1 = new JButton(rsc.getIconDigit1());
+		btnDigit1.setBorder(emptyBorder);
 		btnDigit1.setBounds(0, 20, 40, 40);
 		btnDigit1.addActionListener(new WriteIntoPassword(this, password, 1));
 		panelCodeAccessSupervisor.add(btnDigit1);
 
 		//Button for the digit 2
 		btnDigit2 = new JButton(rsc.getIconDigit2());
+		btnDigit2.setBorder(emptyBorder);
 		btnDigit2.setBounds(40, 20, 40, 40);
 		btnDigit2.addActionListener(new WriteIntoPassword(this, password, 2));
 		panelCodeAccessSupervisor.add(btnDigit2);
 
 		//Button for the digit 3
 		btnDigit3 = new JButton(rsc.getIconDigit3());
+		btnDigit3.setBorder(emptyBorder);
 		btnDigit3.setBounds(80, 20, 40, 40);
 		btnDigit3.addActionListener(new WriteIntoPassword(this, password, 3));
 		panelCodeAccessSupervisor.add(btnDigit3);
 
 		//Button for the digit 4
 		btnDigit4 = new JButton(rsc.getIconDigit4());
+		btnDigit4.setBorder(emptyBorder);
 		btnDigit4.setBounds(0, 60, 40, 40);
 		btnDigit4.addActionListener(new WriteIntoPassword(this, password, 4));
 		panelCodeAccessSupervisor.add(btnDigit4);
 
 		//Button for the digit 5
 		btnDigit5 = new JButton(rsc.getIconDigit5());
+		btnDigit5.setBorder(emptyBorder);
 		btnDigit5.setBounds(40, 60, 40, 40);
 		btnDigit5.addActionListener(new WriteIntoPassword(this, password, 5));
 		panelCodeAccessSupervisor.add(btnDigit5);
 
 		//Button for the digit 6
 		btnDigit6 = new JButton(rsc.getIconDigit6());
+		btnDigit6.setBorder(emptyBorder);
 		btnDigit6.setBounds(80, 60, 40, 40);
 		btnDigit6.addActionListener(new WriteIntoPassword(this, password, 6));
 		panelCodeAccessSupervisor.add(btnDigit6);
 
 		//Button for the digit 7
 		btnDigit7 = new JButton(rsc.getIconDigit7());
+		btnDigit7.setBorder(emptyBorder);
 		btnDigit7.setBounds(0, 100, 40, 40);
 		btnDigit7.addActionListener(new WriteIntoPassword(this, password, 7));
 		panelCodeAccessSupervisor.add(btnDigit7);
 
 		//Button for the digit 8
 		btnDigit8 = new JButton(rsc.getIconDigit8());
+		btnDigit8.setBorder(emptyBorder);
 		btnDigit8.setBounds(40, 100, 40, 40);
 		btnDigit8.addActionListener(new WriteIntoPassword(this, password, 8));
 		panelCodeAccessSupervisor.add(btnDigit8);
 
 		//Button for the digit 9
 		btnDigit9 = new JButton(rsc.getIconDigit9());
+		btnDigit9.setBorder(emptyBorder);
 		btnDigit9.setBounds(80, 100, 40, 40);
 		btnDigit9.addActionListener(new WriteIntoPassword(this, password, 9));
 		panelCodeAccessSupervisor.add(btnDigit9);
 
 		//Button to validate the password
 		btnValidate = new JButton(rsc.getIconCheck());
+		btnValidate.setBorder(emptyBorder);
 		btnValidate.setBounds(0, 140, 40, 40);
 		btnValidate.addActionListener(new ValidatePassword(this, password));
 		panelCodeAccessSupervisor.add(btnValidate);
 
 		//Button for the digit 0
 		btnDigit0 = new JButton(rsc.getIconDigit0());
+		btnDigit0.setBorder(emptyBorder);
 		btnDigit0.setBounds(40, 140, 40, 40);
 		btnDigit0.addActionListener(new WriteIntoPassword(this, password, 0));
 		panelCodeAccessSupervisor.add(btnDigit0);
 
 		//Button to remove the last digit
 		btnCancel = new JButton(rsc.getIconCross());
+		btnCancel.setBorder(emptyBorder);
 		btnCancel.setBounds(80, 140, 40, 40);
 		btnCancel.addActionListener(new CancelPassword(this, password));
 		panelCodeAccessSupervisor.add(btnCancel);
@@ -334,17 +375,20 @@ public class ElevatorView {
 		lblPassword = new JLabel("Tap the password");
 		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPassword.setToolTipText("");
+		lblPassword.setBackground(Color.gray);
 		lblPassword.setBounds(0, 0, 120, 20);
 		panelCodeAccessSupervisor.add(lblPassword);
 
 		panelSupervisor = new JPanel();
 		panelSupervisor.setBounds(360, 40, 182, 500);
-		frame.getContentPane().add(panelSupervisor);
+		panelFont.add(panelSupervisor);
+		panelSupervisor.setBackground(Color.white);
 		panelSupervisor.setVisible(false);
 		panelSupervisor.setLayout(null);
 
 		//Button for POreset
 		btnPOreset = new JButton(rsc.getIconLedOff());
+		btnPOreset.setBorder(emptyBorder);
 		btnPOreset.setBounds(112, 0, 40, 40);
 		btnPOreset.addActionListener(new SendSupPanelReq(mqttPublisher, "manual", "POreset", true));
 		panelSupervisor.add(btnPOreset);
@@ -355,6 +399,7 @@ public class ElevatorView {
 
 		//Button for POdv2
 		btnPOdv2 = new JButton(rsc.getIconLedOff());
+		btnPOdv2.setBorder(emptyBorder);
 		btnPOdv2.setBounds(112, 44, 40, 40);
 		btnPOdv2.addActionListener(new SendSupPanelReq(mqttPublisher, "manual", "POdv2", true));
 		panelSupervisor.add(btnPOdv2);
@@ -365,6 +410,7 @@ public class ElevatorView {
 
 		//Button for POdv1
 		btnPOdv1 = new JButton(rsc.getIconLedOff());
+		btnPOdv1.setBorder(emptyBorder);
 		btnPOdv1.setBounds(112, 90, 40, 40);
 		btnPOdv1.addActionListener(new SendSupPanelReq(mqttPublisher, "manual", "POdv1", true));
 		panelSupervisor.add(btnPOdv1);
@@ -375,6 +421,7 @@ public class ElevatorView {
 
 		//Button for POuv1
 		btnPOuv1 = new JButton(rsc.getIconLedOff());
+		btnPOuv1.setBorder(emptyBorder);
 		btnPOuv1.setBounds(112, 137, 40, 40);
 		btnPOuv1.addActionListener(new SendSupPanelReq(mqttPublisher, "manual", "POuv1", true));
 		panelSupervisor.add(btnPOuv1);
@@ -385,6 +432,7 @@ public class ElevatorView {
 
 		//Button for POuv2
 		btnPOuv2 = new JButton(rsc.getIconLedOff());
+		btnPOuv2.setBorder(emptyBorder);
 		btnPOuv2.setBounds(112, 180, 40, 40);
 		btnPOuv2.addActionListener(new SendSupPanelReq(mqttPublisher, "manual", "POuv2", true));
 		panelSupervisor.add(btnPOuv2);
@@ -395,6 +443,7 @@ public class ElevatorView {
 
 		//Button for POdclose
 		btnPOdclose = new JButton(rsc.getIconLedOff());
+		btnPOdclose.setBorder(emptyBorder);
 		btnPOdclose.setBounds(112, 224, 40, 40);
 		btnPOdclose.addActionListener(new SendSupPanelReq(mqttPublisher, "manual", "POdclose", true));
 		panelSupervisor.add(btnPOdclose);
@@ -405,6 +454,7 @@ public class ElevatorView {
 
 		//Button for POdopen
 		btnPOdopen = new JButton(rsc.getIconLedOff());
+		btnPOdopen.setBorder(emptyBorder);
 		btnPOdopen.setBounds(112, 274, 40, 40);
 		btnPOdopen.addActionListener(new SendSupPanelReq(mqttPublisher, "manual", "POdopen", true));
 		panelSupervisor.add(btnPOdopen);
@@ -440,6 +490,7 @@ public class ElevatorView {
 		labelTable.put( 5, new JLabel("5") );
 		sliderPOv_crawlSelect.setLabelTable(labelTable);
 		sliderPOv_crawlSelect.setPaintLabels(true);
+		sliderPOv_crawlSelect.setBackground(Color.white);
 		sliderPOv_crawlSelect.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
